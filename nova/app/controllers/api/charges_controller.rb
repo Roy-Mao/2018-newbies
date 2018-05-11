c6a5e563e2c52b886803f4292bda1e030fb448f1
4geru <uchinishi.koichi@moneyforward.co.jp>
2018-05-11 15:25:16 +0900

    hoge

--- nova/app/controllers/api/charges_controller.rb
+++ nova/app/controllers/api/charges_controller.rb
@@ -2,19 +2,6 @@
 
 class Api::ChargesController < Api::ApplicationController
   def index
-    # charge_total = 0
-    # current_user.charges.each do |charge|
-    #   charge_total += charge.amount
-    # end
-    # remit_total = 0
-    # current_user.received_remit_requests.where.not(accepted_at: nil).each do |remit|
-    #   remit_total += remit.amount
-    # end
-    # current_user.sent_remit_requests.where.not(accepted_at: nil).each do |remit|
-    #   remit_total -= remit.amount
-    # end
-    # balance_total = charge_total - remit_total
-
     @charges = current_user.charges.order(id: :desc).limit(50)
 
     render json: { charges: @charges }
