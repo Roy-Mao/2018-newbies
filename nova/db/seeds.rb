7b06467a315a91cb3d99c934dcfe31c843ae6090
4geru <uchinishi.koichi@moneyforward.co.jp>
2018-05-14 12:27:26 +0900

    add user, remit_request seed file

--- nova/db/seeds.rb
+++ nova/db/seeds.rb
@@ -6,3 +6,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({
+  nickname: 'geru',
+  password: 'h0gehoge',
+  email: 'geru@gmail.com',
+  activated: true,
+  activated_at: DateTime.now
+})
+
+User.create({
+  nickname: 'hoge',
+  password: 'h0gehoge',
+  email: 'hoge@gmail.com',
+  activated: true,
+  activated_at: DateTime.now
+})
+
+20.times.each do |i|
+  RemitRequest.create(user: User.first, target: User.second, amount: 100)
+  RemitRequest.create(user: User.second, target: User.first, amount: 200)
+end
