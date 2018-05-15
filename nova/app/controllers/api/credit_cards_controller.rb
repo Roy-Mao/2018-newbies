5037588dc2a4210741179cb332b16b46cee02459
毛 若愚 <ruoyu.mao@icloud.com>
2018-05-15 17:13:39 +0900

    エラーメッセージ表示の色を赤くなりました

--- nova/app/controllers/api/credit_cards_controller.rb
+++ nova/app/controllers/api/credit_cards_controller.rb
@@ -9,6 +9,7 @@ class Api::CreditCardsController < Api::ApplicationController
   def create
     User.transaction do
       current_user.credit_card.try(:destroy)
+      raise 'something wrong'
       @credit_card = CreditCard.create!(credit_card_params.merge(user: current_user))
       render json: @credit_card.as_json(only: %i[last4]), status: :created
 
