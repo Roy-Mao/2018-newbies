b936bc954694e23aef1bb95b2ee308403837d06f
yuna iwamura <iwamura.yuna@moneyforward.co.jp>
2018-05-15 10:20:18 +0900

    バリデーションの修正

--- nova/app/models/remit_request.rb
+++ nova/app/models/remit_request.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class RemitRequest < ApplicationRecord
+  MIN_REMIT_AMOUNT = 1
+
+  validates :amount, numericality: { greater_than_or_equal_to: MIN_REMIT_AMOUNT, only_integer: true }, presence: true
+
   belongs_to :user
   belongs_to :target, class_name: 'User'
 
