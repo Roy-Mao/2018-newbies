60809946d785e96dc424abde14bd24df5eabc8c3
毛 若愚 <ruoyu.mao@icloud.com>
2018-05-17 18:44:29 +0900

    1000->1000confirm money

--- nova/app/models/remit_request.rb
+++ nova/app/models/remit_request.rb
@@ -14,15 +14,15 @@ class RemitRequest < ApplicationRecord
   enum status: { outstanding: 0, accepted: 1, rejected: 2, canceled: 3 }
 
   def change_status_accept
-    enough_money = self.target.amount > self.amount
-    if self.status == 'outstanding' && enough_money
-       self.status = :accepted
-       self.user.amount += self.amount
-       self.target.amount -= self.amount
+    enough_money = self.target.amount >= self.amount
+    if self.status == 'outstanding' && enough_money 
+      self.status = :accepted
+      self.user.amount += self.amount
+      self.target.amount -= self.amount
+      self.save!
+      self.user.save!
+      self.target.save!
     end
-    self.save!
-    self.user.save!
-    self.target.save!
     self.target.amount
   end
 
