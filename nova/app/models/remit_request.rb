a38f45cf4abde814be88d56464da63fbe674a5e1
yuna iwamura <iwamura.yuna@moneyforward.co.jp>
2018-05-17 12:38:23 +0900

    typo fix

--- nova/app/models/remit_request.rb
+++ nova/app/models/remit_request.rb
@@ -8,7 +8,7 @@ class RemitRequest < ApplicationRecord
   belongs_to :user
   belongs_to :target, class_name: 'User'
 
-  validates :amount, numericality: { greater_then: 0 }
+  validates :amount, numericality: { greater_than: 0 }
   paginates_per 10
 
   enum status: { outstanding: 0, accepted: 1, rejected: 2, canceled: 3 }
